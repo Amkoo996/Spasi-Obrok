8f2d94b145d6a368b3e39fe0a12163cfaa890f3c
Amkoo996 <89833732+Amkoo996@users.noreply.github.com>
2026-04-22 21:50:30 +0200

    feat: Initialize Firebase analytics safely
    Integrates Firebase analytics with safe initialization to handle environments where it might not be supported. Also, adjusts Firestore initialization to use the default instance as the user configuration does not specify a `firestoreDatabaseId`.

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,11 +1,29 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import firebaseConfig from '../firebase-applet-config.json';
+import { initializeApp } from "firebase/app";
+import { getAnalytics, isSupported } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAIRdkZV2LYXiUTIj8HOifWvwy-ldNawdQ",
+  authDomain: "spasiobrok.firebaseapp.com",
+  projectId: "spasiobrok",
+  storageBucket: "spasiobrok.firebasestorage.app",
+  messagingSenderId: "640149044788",
+  appId: "1:640149044788:web:1d1e00ef9db9dccb86d461",
+  measurementId: "G-3TDJ8G9SFP"
+};
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+// Initialize Analytics safely (in case it's not supported in some environments/iframes)
+isSupported().then(supported => {
+  if (supported) {
+    getAnalytics(app);
+  }
+});
+
+// Since user config doesn't have firestoreDatabaseId, we use the default
+export const db = getFirestore(app);
 export const auth = getAuth(app);
 
 const provider = new GoogleAuthProvider();
